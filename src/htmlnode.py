0b4775708dc1636a2a55c6744d1dc71fff7d54a0
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-09-02 20:31:56 -0400

    Initialize LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -26,5 +26,18 @@ class HTMLNode:
         if self.props:
             for k, v in self.props.items():
                 result.append(f"{k}=\"{v}\"")
-        return " ".join(result)
+            return " " + " ".join(result)
+        return ""
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag=tag,value=value,props=props)
+        self.tag = tag
+        self.value = value
+        self.props = props
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("All leaf nodes must have a value.")
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,16 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
     def test_eq(self):
         node = HTMLNode("h1", "Header1")
         self.assertEqual(str(node), "h1 Header1")
+    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
 
 if __name__ == "__main__":
     unittest.main()
