a27b198b15cb16282a52f04b87fb67d0e31b22d8
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-09-04 00:46:41 -0400

    Node Handling Bugfixes

--- src/textnode.py
+++ src/textnode.py
@@ -60,23 +60,23 @@ def split_nodes_delimiter(
         delimiter: DelimiterType,
         text_type: TextType
 ):
-    result = []
-    for node in old_nodes:
-        if delimiter.value not in node.text:
-            result.append(node)
+    for i in range(len(old_nodes)):
+        if delimiter.value not in old_nodes[i].text:
+            continue
 
-        split_string = node.text.split(delimiter.value)
+        split_string = old_nodes[i].text.split(delimiter.value)
 
         if len(split_string) % 2 == 0:
             raise Exception("Closing delimiter not found.")
 
-        for i in range(len(split_string)):
+        new_items = []
+        for j in range(len(split_string)):
             node_type = text_type
-            if i % 2 == 0:
+            if j % 2 == 0:
                 node_type = TextType.TEXT
-            result.append(TextNode(split_string[i], node_type))
-
-    return result
+            new_items.append(TextNode(split_string[j], node_type))
+        old_nodes[i:i+1] = new_items
+    return old_nodes
 
 def extract_markdown_images(text: str):
     pattern = r"(?:!)\[(.*?)\]\((.*?)\)"
@@ -94,7 +94,7 @@ def __handle_sections(
     result = []
     lead_char = ""
     text_type = TextType.LINK
-    search_text = node.text
+    remaining_text = node.text
 
     if is_image:
         lead_char = "!"
@@ -102,34 +102,34 @@ def __handle_sections(
 
     for i in range(len(links)):
         link_text, link_url = links[i]
-        sections = search_text.split(f"{lead_char}[{link_text}]({link_url})",1)
+        sections = remaining_text.split(f"{lead_char}[{link_text}]({link_url})",1)
         for j in range(len(sections)):
             if j % 2 != 0:
                 result.append(TextNode(link_text, text_type, link_url))
                 continue
             result.append(TextNode(sections[j], TextType.TEXT))
-        search_text = sections[-1]
+        remaining_text = sections[-1]
+    if remaining_text != "":
+        result.append(TextNode(remaining_text, TextType.TEXT))
     return result
 
 def split_nodes_image(old_nodes: list[TextNode]):
-    result = []
-    for node in old_nodes:
-        image_links = extract_markdown_images(node.text)
+    for i in range(len(old_nodes)):
+        image_links = extract_markdown_images(old_nodes[i].text)
         if len(image_links) == 0:
-            result.append(node)
             continue
-        new_nodes = __handle_sections(node, image_links, True)
-        result.extend(new_nodes)
-    return result
+
+        new_nodes = __handle_sections(old_nodes[i], image_links, True)
+        old_nodes[i:i+1] = new_nodes
+    return old_nodes
 
 def split_nodes_link(old_nodes: list[TextNode]):
-    result = []
-    for node in old_nodes:
-        links = extract_markdown_links(node.text)
+    for i in range(len(old_nodes)):
+        links = extract_markdown_links(old_nodes[i].text)
         if len(links) == 0:
-            result.append(node)
             continue
-        new_nodes = __handle_sections(node, links)
-        result.extend(new_nodes)
-    return result
+
+        new_nodes = __handle_sections(old_nodes[i], links)
+        old_nodes[i:i+1] = new_nodes
+    return old_nodes
 
