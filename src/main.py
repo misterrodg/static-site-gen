ce83b2cf43b293ea3ca3c017dd8c068902c8b0b6
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-09-04 22:45:18 -0400

    Initialize Deploy Script

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+def deploy():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(base_dir)
+
+    public_dir = os.path.join(root_dir, "public")
+    static_dir = os.path.join(root_dir, "static")
+
+    for item in os.listdir(public_dir):
+        item_path = os.path.join(public_dir, item)
+        if os.path.isfile(item_path) or os.path.islink(item_path):
+            os.remove(item_path)
+        elif os.path.isdir(item_path):
+            shutil.rmtree(item_path)
+
+    shutil.copytree(static_dir, public_dir, dirs_exist_ok=True)
 
 def main():
-    example = TextNode(
-        "This is some anchor text",
-        TextType.LINK,
-        "https://www.boot.dev"
-    )
-    print(example)
+    deploy()
 
 if __name__ == "__main__":
     main()
